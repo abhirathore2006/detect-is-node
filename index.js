a46a5002913adf895e7a05e93d608576f662acbd
Abhimanyu Singh Rathore <abhirathore2006@gmail.com>
2017-02-23 16:03:36 +0530

    changed eval to actual function code to fix issue #1

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-var isNode=new Function("try {return this===global;}catch(e){return false;}");
-
-// tests if global scope is binded to "global"
-//if(isNode()) console.log("running under node.js");
-
+//var isNode=new Function("try {return this===global;}catch(e){return false;}");
+var isNode =function(){
+    var windowTest;
+     try {
+        //if window is declared thorugh JS DOM then window will be defined but will not be equal to this
+        windowTest  =  this === window;
+     }catch(e) {
+       return true;
+     }
+      try {
+        return this===global && !windowTest;
+      }
+      catch(e) {
+        return false;
+      }
+}
 module.exports = isNode;
